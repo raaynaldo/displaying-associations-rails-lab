34344d8af56253c9dd20d9f20b5f48e46d5dbe57
raaynaldo <raaynaldo@hotmail.com>
2020-11-01 00:07:37 -0400

    commit tree 5def651cfe01e97a4567c05af034bc474436a3a2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.destroy_all
+Song.destroy_all
+
+kp = Artist.create(name: "Katy Perry")
+Song.create(title: "Fireworks", artist: kp)
+Song.create(title: "Roar", artist: kp)
+Song.create(title: "Dark Horse", artist: kp)
+Song.create(title: "Con Calma", artist: kp)
